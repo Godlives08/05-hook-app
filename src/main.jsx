82e003197bc354e5e4b90a7d17f94d4dde06f0e3
Godlives08 <manuel.astacio@outlook.com>
2023-05-05 15:50:10 -0400

    Hook useState

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
 import { HookApp } from './hookApp'
+import { CounterApp } from './01-useState/CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HookApp />
+    <CounterApp />
   </React.StrictMode>,
 )
