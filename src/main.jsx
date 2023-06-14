83b11786df25ab08884a6da10e108406e97f956a
Manuel Astacio <manuel.astacio@outlook.com>
2023-06-14 08:12:43 -0400

    Intro-useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
 import './index.css'
 
+import './08-useReducer/intro-Reducer'
+
 // import { HookApp } from './hookApp'
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
@@ -15,7 +17,7 @@ import './index.css'
 // import { Memorize } from './06-memos/Memorize';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { CallbackHook } from './06-memos/CallbackHook';
-import { Padre } from './07-tarea-memo/Padre';
+// import { Padre } from './07-tarea-memo/Padre';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -46,7 +48,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* Practica 04 
     <FocusScreen />  */}
 
-    <Padre />
+    {/* <Padre /> */}
 
   </>
   // </React.StrictMode>
