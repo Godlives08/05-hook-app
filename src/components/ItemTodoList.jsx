e6a7187b87686655f0d33a276f89545592c1f455
Manuel Astacio <manuel.astacio@outlook.com>
2023-06-18 21:30:12 -0400

    Update ItemTodoList.jsx

--- src/components/ItemTodoList.jsx
+++ src/components/ItemTodoList.jsx
@@ -1,9 +1,9 @@
 export const ItemTodoList = ({todo}) => {
     return (
         <li className="list-group-item">
-            <input className="form-check-input me-1 mt-2" type="checkbox" value="" aria-label="..." />
+            {todo.done?`✅`:`⬜`}
             <span className="align-self-center ms-3">{todo.description}</span>
-            <button className="btn btn-danger float-end">Borrar</button>
+            <button className="btn btn-outline-danger float-end">✖️</button>
         </li>
     )
 }
