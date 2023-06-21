d7db7daa8ba3ea7a0fed4447070c0ccb3e27dcee
Manuel Astacio <manuel.astacio@outlook.com>
2023-06-21 08:53:47 -0400

    Nav bar agregado

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,34 +1,12 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Link, Navigate, Route, Routes } from "react-router-dom"
 import { AboutPage, HomePage, LoginPage } from "./"
 import { MultipleCustomHooks } from "../03-examples/MultipleCustomHooks"
+import { NavBar } from "../components"
 
 export const MainApp = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Home</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link" href="/about">About</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/login">Login</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/pokedex">Pokedex</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled" href="#" aria-disabled="true">Disabled</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <NavBar />
             <hr />
             <Routes>
                 <Route path="/" element={<HomePage />} />
